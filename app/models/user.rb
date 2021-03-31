0c22cec14b5ee22f53ba217dc3a76ad2df577e0c
kuz-en <bio19@ya.ru>
2021-03-31 23:13:38 +0300

    Fix styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 require 'openssl'
 
 class User < ApplicationRecord
-
+  VALID_USERNAME = /\A\w+\z/.freeze
+  VALID_EMAIL = /.+@.+\..+/i.freeze
   ITERATIONS = 20_000
   DIGEST = OpenSSL::Digest.new('SHA256')
 
@@ -9,30 +10,21 @@ class User < ApplicationRecord
 
   has_many :questions
 
-  validates :username, length: { maximum: 40 }
-  validates :username, format: { with: /\A\w+\z/ }
+  validates :username,
+            presence: true,
+            uniqueness: true,
+            length: { maximum: 40 },
+            format: { with: VALID_USERNAME }
 
-  validates :email, :username, presence: true
-  validates :email, :username, uniqueness: true
-
-  validates :email, format: { with: /.+@.+\..+/i }
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            format: { with: VALID_EMAIL }
 
   validates :password, presence: true, on: :create
   validates :password, confirmation: true
 
   before_save :encrypt_password
-
-  def encrypt_password
-    if password.present?
-      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
-
-      self.password_hash = User.hash_to_string(
-        OpenSSL::PKCS5.pbkdf2_hmac(
-          password, password_salt, ITERATIONS, DIGEST.length, DIGEST
-        )
-      )
-    end
-  end
 
   def self.hash_to_string(password_hash)
     password_hash.unpack1('H*')
@@ -48,6 +40,21 @@ class User < ApplicationRecord
       )
     )
     return user if user.password_hash == hashed_password
+
     nil
+  end
+
+  private
+
+  def encrypt_password
+    if password.present?
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+
+      self.password_hash = User.hash_to_string(
+        OpenSSL::PKCS5.pbkdf2_hmac(
+          password, password_salt, ITERATIONS, DIGEST.length, DIGEST
+        )
+      )
+    end
   end
 end
