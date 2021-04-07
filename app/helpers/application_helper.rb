994985fb677d144b240fe0a54303d8b4608ac00f
kuz-en <bio19@ya.ru>
2021-04-07 13:18:10 +0300

    fix user_avatar helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   def user_avatar(user)
-    user.avatar_url.present? || asset_pack_path('media/images/avatar.jpg')
+    user.avatar_url = nil if user.avatar_url == ''
+    user.avatar_url || asset_pack_path('media/images/avatar.jpg')
   end
 
   def incline(quantity, one, two, five)
